978b8f40ede62e20b0348ee1629a316a4f87191c
raigal-r <raquel.raigal@outlook.es>
2023-08-08 02:51:02 +0300

    onClick added

--- peerlocal/src/pages/landing.tsx
+++ peerlocal/src/pages/landing.tsx
@@ -17,6 +17,11 @@ const Landing: NextPage = () => {
   const commMeta = useCommunityIPFS({
     ipfsHash: h,
   });
+
+  const LandingPage = () => {
+    const handleButtonClick = () => {
+      window.location.href = 'https://pee-rwallet-zkeq.vercel.app/landing/2';
+    };
   {
     /*
   console.log(community.data?.community?.ipfsMetadata)
@@ -51,7 +56,7 @@ const Landing: NextPage = () => {
               <h2 className="card-title">Decisions</h2>
               <p>Vote now for existing proposals in our Community DAO</p>
               <div className="card-actions">
-                <button onClick="https://pee-rwallet-zkeq.vercel.app/landing/2" className="btn-inactive btn">GO TO VOTE</button>
+                <button onClick={handleButtonClick} className="btn-inactive btn">GO TO VOTE</button>
               </div>
             </div>
           </div>
